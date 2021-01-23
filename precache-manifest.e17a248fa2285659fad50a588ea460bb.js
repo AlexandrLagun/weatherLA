5e5d25aa7fee9018fa63d98bedc9a9e04e476713
AlexandrLagun <alexanderlagun717@gmail.com>
2021-01-23 23:42:27 +0300

    Updates

---
+++ precache-manifest.e17a248fa2285659fad50a588ea460bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eef135bf8f08f192f319022debc2de9",
+    "url": "/weatherLA/index.html"
+  },
+  {
+    "revision": "3b3b3f1c62e1112a334f",
+    "url": "/weatherLA/static/css/main.00637221.chunk.css"
+  },
+  {
+    "revision": "f9ac637b6774ee54c693",
+    "url": "/weatherLA/static/js/2.b890f74e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weatherLA/static/js/2.b890f74e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b3b3f1c62e1112a334f",
+    "url": "/weatherLA/static/js/main.cde8587e.chunk.js"
+  },
+  {
+    "revision": "759a2009cb06883d12d6",
+    "url": "/weatherLA/static/js/runtime-main.9b6bc691.js"
+  }
+]);
